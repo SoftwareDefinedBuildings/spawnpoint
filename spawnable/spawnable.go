f478369181bbdd17e5ebf3eca6da3ea686315044
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-04-06 00:27:26 -0700

    Update spawnable

--- spawnable/spawnable.go
+++ spawnable/spawnable.go
@@ -1,16 +1,22 @@
 package spawnable
 
 import (
+	"bytes"
 	"encoding/base64"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
-func GetParams() (map[string]interface{}, error) {
+type Params struct {
+	dat map[string]interface{}
+}
+
+func GetParams() (*Params, error) {
 	params := make(map[string]interface{})
 	paramContents, err := ioutil.ReadFile("params.yml")
 	if err != nil {
@@ -20,9 +26,9 @@ func GetParams() (map[string]interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	return params, nil
+	return &Params{params}, nil
 }
-func GetParamsOrExit() map[string]interface{} {
+func GetParamsOrExit() *Params {
 	params, err := GetParams()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not read params.yml: %v\n", err)
@@ -30,7 +36,8 @@ func GetParamsOrExit() map[string]interface{} {
 	}
 	return params
 }
-func GetEntity(params map[string]interface{}) (blob []byte, err error) {
+
+func (p *Params) GetEntity() (blob []byte, err error) {
 	v, ok := params["entityx"]
 	if !ok {
 		return nil, errors.New("Could not decode entity\n")
@@ -45,11 +52,57 @@ func GetEntity(params map[string]interface{}) (blob []byte, err error) {
 	}
 	return eblob, nil
 }
-func GetEntityOrExit(params map[string]interface{}) (blob []byte) {
+func (p *Params) GetEntityOrExit() (blob []byte) {
 	blob, err := GetEntity(params)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not get entity from params: %v\n", err)
 		os.Exit(1)
 	}
 	return blob
+}
+func (p *Params) MustString(key string) string {
+	rv, ok := p.dat[key]
+	if !ok {
+		fmt.Printf("Missing paramater '%s'\n", key)
+	}
+	rvs, ok := rv.(string)
+	if !ok {
+		fmt.Printf("Parameter '%s' should be a string\n", key)
+	}
+	return rvs
+}
+func (p *Params) MustInt(key string) int {
+	rv, ok := p.dat[key]
+	if !ok {
+		fmt.Printf("Missing paramater '%s'\n", key)
+	}
+	rvi, ok := rv.(int)
+	if !ok {
+		fmt.Printf("Parameter '%s' should be an int\n", key)
+	}
+	return rvi
+}
+
+func DoHttpPutStr(uri, content string, headers []string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT",
+		uri, bytes.NewBufferString(content))
+	if err != nil {
+		fmt.Println("Got HTTP PUT error: ", err)
+		return ""
+	}
+	if len(headers)%2 != 0 {
+		fmt.Println("Odd number of headers")
+		return ""
+	}
+	for i := 0; i < len(headers)/2; i += 2 {
+		req.Header.Add(headers[i], headers[i+1])
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Got HTTP PUT error: ", err)
+		return ""
+	}
+	contents, _ := ioutil.ReadAll(resp.Body)
+	return string(contents)
 }
