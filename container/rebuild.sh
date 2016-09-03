b7f2497eedb270fb67cc1f4cc7d58164cb000847
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-09-02 19:46:12 -0700

    refactor containers

--- container/rebuild.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cp ../spawnd/spawnd .
-docker build . --no-cache -t jhkolb/spawnd
-docker push jhkolb/spawnd
---
+++ containers/rebuild.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+${REPO_PREFIX:=immesys}
+
+# Spawnable amd64
+pushd spawnable/amd64
+docker build --pull --no-cache -t $REPO_PREFIX/spawnpoint:amd64 .
+docker push $REPO_PREFIX/spawnpoint:amd64
+popd
+
+# Spawnable armv7
+pushd spawnable
+rsync -PHav --delete armv7/ ubuntu@10.4.10.200:~/spawnpointbuild
+ssh ubuntu@10.4.10.200 "cd spawnpointbuild && docker build --pull --no-cache -t $REPO_PREFIX/spawnpoint:armv7 . && docker push $REPO_PREFIX/spawnpoint:armv7"
+popd
+
+# Spawnd amd64
+cp ../spawnd/spawnd spawnd/amd64/
+pushd spawnd/amd64
+docker build --pull --no-cache -t $REPO_PREFIX/spawnd:amd64 .
+docker push $REPO_PREFIX/spawnd:amd64
+popd
+
+# Spawnd armv7
+pushd spawnd
+rsync -PHav --delete ../../ ubuntu@10.4.10.200:~/spawndbuild
+ssh ubuntu@10.4.10.200 "cd spawndbuild/spawnd && TMDIR=/home/ubuntu/tmp go get -d ./... && TMPDIR=/home/ubuntu/tmp go build . && cp spawnd ../containers/spawnd/armv7 && cd ../containers/spawnd/armv7 && docker build --pull --no-cache -t $REPO_PREFIX/spawnd:armv7 . && docker push $REPO_PREFIX/spawnd:armv7"
+popd
